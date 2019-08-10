26a792c0f12b11801d365d9088051da2529d0557
YongjieXue <yongjiexue@YongjieXuedeMacBook-Pro.local>
2019-08-10 21:08:17 +0800

    finfish master

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React,{Component} from 'react';
+import {BrowserRouter,Route} from 'react-router-dom'
+import Index from './pages/index'
+import Main from './pages/main'
+class App extends Component{
+  render(){
+    return (
+     <BrowserRouter>
+        <Route path='/' exact  component={Index} ></Route>
+        <Route path='/main' exact  component={Main} ></Route>
+     </BrowserRouter>
+    )
+  }
 }
 
 export default App;
